921a7bfeb6288487b1a9ad7f27a6583bf9dc8a8b
Giulia Martini <martini.giulia94@gmail.com>
2025-07-31 19:20:21 +0200

    Chore: wip first analysos

--- src/codab.py
+++ src/codab.py
@@ -1,20 +1,16 @@
 import os
-from pathlib import Path
 
 import geopandas as gpd
 
 AA_DATA_DIR = os.getenv("AA_DATA_DIR")
 
 def load_codab(admin_level: int = 0):
-    if admin_level not in [0,1,2]:
+    AA_DATA_DIR = os.getenv("AA_DATA_DIR")
+    if admin_level not in [0,1,2,3]:
         raise ValueError("Only admin level 1 is supported")
-    adm_path = (
-        Path(AA_DATA_DIR)
-        / "public"
-        / "raw"
-        / "mmr"
-        / "cod_ab"
-        / "mmr_adm_250k_mimu_20240215_ab_shp.zip"
-    )
+    if admin_level in [0,1,2, 3]:
+        adm_path = os.path.join(AA_DATA_DIR,
+            "public/raw/mmr/cod_ab/mmr_adm_250k_mimu_20240215_ab_shp.zip"
+        )
     gdf = gpd.read_file(adm_path, layer = f"mmr_polbnda_adm{admin_level}_250k_mimu_20240215")
     return gdf
